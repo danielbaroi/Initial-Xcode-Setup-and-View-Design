3d35d9324a497e2b421289f9126d6a23ff36523f
Daniel Baroi <danielbaroi@cedarville.edu>
2024-09-11 15:39:11 -0400

    Initial Commit

---
+++ Initial Xcode Setup and View Design/Initial_Xcode_Setup_and_View_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Initial_Xcode_Setup_and_View_DesignApp.swift
+//  Initial Xcode Setup and View Design
+//
+//  Created by Daniel Baroi on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Initial_Xcode_Setup_and_View_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
